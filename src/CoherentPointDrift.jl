843eb6522e40491733ac167f4ec5b85dcaaf7c6d
CorySimon <corysimon@berkeley.edu>
2018-10-12 17:36:39 -0700

    hmm \sigma2 goes to less than zero...

--- src/CoherentPointDrift.jl
+++ src/CoherentPointDrift.jl
@@ -25,6 +25,7 @@ function q_objective(X::Array{Float64, 2}, Y::Array{Float64, 2}, P::Array{Float6
     D = size(X)[1]
     M = size(Y)[2]                                                                          
     N = size(X)[2]
+    Np = sum(P)
 
     Y_transformed = R * Y .+ t
 
@@ -35,14 +36,14 @@ function q_objective(X::Array{Float64, 2}, Y::Array{Float64, 2}, P::Array{Float6
         end
     end
     q /= 2 * σ²
-    q += N * D / 2 * log(σ²)
+    q += Np * D / 2 * log(σ²)
     return q
 end
 
 "What transformation R * Y + t needs to be applied to Y to make it match X?"
-function affine_point_set_registration(X::Array{Float64, 2}, Y::Array{Float64, 2};
+function rigid_point_set_registration(X::Array{Float64, 2}, Y::Array{Float64, 2};
                                        allow_translation::Bool=false,
-                                       nb_em_steps::Int=5)
+                                       nb_em_steps::Int=5, w::Float64=0.0)
     # nb of data pts, dimension
     D = size(X)[1]
     @assert D == size(Y)[1] "data must be of same dimension"
@@ -56,7 +57,7 @@ function affine_point_set_registration(X::Array{Float64, 2}, Y::Array{Float64, 2
     # the bandwidth (variance) of the Gaussian mixture model
     σ² = initial_σ²(X, Y)
     # probabilities of correspondence
-    P = rand(Float64, M, N)
+    P = zeros(Float64, M, N)
     q = 0.0
 
     for em_step = 1:nb_em_steps
@@ -70,13 +71,13 @@ function affine_point_set_registration(X::Array{Float64, 2}, Y::Array{Float64, 2
                 P[m, n] = exp(-sum((Y_transformed[:, m] - X[:, n]) .^ 2) / (2 * σ²))
             end
             # at this pt row m is filled
-            P[:, n] = P[:, n] / sum(P[:, n])
+            P[:, n] = P[:, n] / (sum(P[:, n]) + (2*π*σ²)^(D/2) * w / (1.0 - w) * M / N)
         end
-        @assert isapprox(sum(P), N, atol=0.05)
+        Np = sum(P)
         
         # M step. Get best transformation
-        μx = X * transpose(P) * ones(M) / N
-        μy = Y * P * ones(N) / N
+        μx = (sum(X * transpose(P), dims=2) / Np)[:]
+        μy = (sum(Y * P, dims=2) / Np)[:]
         Xhat = broadcast(-, X, μx)
         Yhat = broadcast(-, Y, μy)
 
@@ -86,22 +87,21 @@ function affine_point_set_registration(X::Array{Float64, 2}, Y::Array{Float64, 2
         
         R = F.U * diagm(0 => [i == D ? det(F.U * F.Vt) : 1.0 for i = 1:D]) * F.Vt
 
-        σ² = (tr(Xhat * diagm(0 => transpose(P) * ones(M)) * transpose(Xhat)) - tr(transpose(A) * R)) / (N * D)
+        σ² = (tr(Xhat * diagm(0 => transpose(P) * ones(M)) * transpose(Xhat)) - tr(transpose(A) * R)) / (Np * D)
 
         # objective
         q = q_objective(X, Y, P, σ², R, t)
         println("\t\tobjective: ", q)
+        println("\t\tσ² = ", σ²)
 
         if allow_translation
             t = μx .- R * μy
         end
     end
 
-    println("σ = ", sqrt(σ²))
-
     return R, t, σ², q
 end
 
-export affine_point_set_registration
+export rigid_point_set_registration
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,26 +13,43 @@ color_palette = Scale.color_discrete().f(7)[[1, 4, 6]]
 themes = [Theme(default_color=color, panel_fill="white", grid_color="lightgray", 
                 background_color=parse(Colorant, "white")) for color in color_palette]
 
+ # # to test arrays of different sizes and find absurd bugs
+ # rigid_point_set_registration(rand(3, 10), rand(3, 15), allow_translation=true, nb_em_steps=4, w=0.2)
+ # rigid_point_set_registration(rand(3, 15), rand(3, 10), allow_translation=true, nb_em_steps=4, w=0.2)
+
+function test_transform(Y::Array{Float64, 2}, θ::Float64, t::Array{Float64, 1}, noise_level::Float64)
+    R = rotation_matrix2d(θ)
+    return R * Y .+ t .+ noise_level * randn(size(Y))
+end
+
 @testset "2D visual test" begin
     # read in a 2D point cloud
     Y = collect(readdlm("osu.csv")')
     
-    # rotate at a known angle
-    θ = 2 * π * rand()
+    ###
+    #  rotate at a known angle, recover rotation
+    ###
+    θ = π / 6.0
     R_known = rotation_matrix2d(θ)
-    t_known = [rand(), rand()]
+    X = test_transform(Y, θ, [0.0, 0.0], 0.0)
+    R, t, σ², q = rigid_point_set_registration(X, Y, allow_translation=false, nb_em_steps=4, w=0.2)
+    @test isapprox(R, R_known, atol=0.001)
 
-    # construct X as a known transformation of Y
-    X = R_known * Y .+ t_known .+ 0.00001 * randn(size(Y))
-    
+    ###
+    #  translate and rotate
+    ###
+    t_known = [0.25, 0.45]
+    X = test_transform(Y, θ, t_known, 0.0)
     # see if we can recover the rotation matrix
-    R, t, σ², q = affine_point_set_registration(X, Y, allow_translation=true, nb_em_steps=15)
+    R, t, σ², q = rigid_point_set_registration(X, Y, allow_translation=true, nb_em_steps=15, w=0.2)
     @test isapprox(R, R_known, atol=0.001) # well, need -θ rotation to rotate back.
     @test isapprox(t, t_known, atol=0.001)
-
-    # a visual test; when we add noise
-    X = R_known * Y .+ t_known .+ 0.01 * randn(size(X))
     
+    ###
+    #   now with some noise (and plot)
+    ###
+    X = test_transform(Y, θ, t_known, 0.01)
+
     # plot original points and points rotated, with noise.
     p = plot(layer(x=X[1, :], y=X[2, :], Geom.point, themes[1]),
              layer(x=Y[1, :], y=Y[2, :], Geom.point, themes[2]),
@@ -41,7 +58,7 @@ themes = [Theme(default_color=color, panel_fill="white", grid_color="lightgray",
        )
     draw(PNG("before_alignment.png", 5inch, 4inch, dpi=300), p)
 
-    R, t, σ², q = affine_point_set_registration(X, Y, nb_em_steps=8, allow_translation=true)
+    R, t, σ², q = rigid_point_set_registration(X, Y, allow_translation=true, nb_em_steps=15, w=0.2)
     Y_transformed = R * Y .+ t
 
     p = plot(layer(x=X[1, :], y=X[2, :], Geom.point, themes[1]),
