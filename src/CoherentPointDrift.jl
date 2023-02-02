0a4f7dd723f03e96e2d483c5a7570eb21d752a9e
Max Freudenberg <max.freudenberg@protonmail.com>
2023-02-02 11:36:33 +0100

    speed up, remove DelimitedFiles

--- src/CoherentPointDrift.jl
+++ src/CoherentPointDrift.jl
@@ -1,7 +1,6 @@
 module CoherentPointDrift
 
 using LinearAlgebra
-using DelimitedFiles
 using Printf
 
 function dmn(X::Array{Float64, 2}, Y::Array{Float64, 2})
@@ -43,7 +42,16 @@ function q_objective(X::Array{Float64, 2}, Y::Array{Float64, 2}, P::Array{Float6
     return q
 end
 
-"What transformation R * Y + t needs to be applied to Y to make it match X?"
+"""
+    rigid_point_set_registration(X::Array{Float64, 2}, Y::Array{Float64, 2};
+                                 allow_translation, max_nb_em_steps,
+                                 q_tol, w, verbose, print_ending, σ²_tol)
+
+What transformation R * Y + t needs to be applied to Y to make it match X?
+
+# Returns
+    rotation matrix, translation vector, variance, error
+"""
 function rigid_point_set_registration(X::Array{Float64, 2}, Y::Array{Float64, 2};
                                       allow_translation::Bool=false,
                                       max_nb_em_steps::Int=25, q_tol::Float64=1e-5,
@@ -80,10 +88,10 @@ function rigid_point_set_registration(X::Array{Float64, 2}, Y::Array{Float64, 2}
         # E step. Get correspondence.
         for n = 1:N
             for m = 1:M
-                @inbounds P[m, n] = exp(-sum((Y_transformed[:, m] - X[:, n]) .^ 2) / (2 * σ²))
+                @inbounds @views P[m, n] = exp(-sum(x->x^2, (Y_transformed[:, m] - X[:, n])) / (2 * σ²))
             end
             # at this pt row m is filled
-            @inbounds P[:, n] .= P[:, n] / (sum(P[:, n]) + (2*π*σ²)^(D/2) * w / (1.0 - w) * M / N)
+            @inbounds P[:, n] ./= sum(P[:, n]) + (2*π*σ²)^(D/2) * w / (1.0 - w) * M / N
         end
         Np = sum(P)
         
